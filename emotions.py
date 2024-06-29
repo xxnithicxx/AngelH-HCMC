82985518c5b52629b4f0e483f3bb8f60d94160ba
BME-AIEnthusiast <bebeiu20220@gmail.gmailcom>
2024-06-30 00:43:59 +0700

    feat: ⚡ init the emotion capture api

--- emotions.py
+++ emotions.py
@@ -1,10 +1,6 @@
-# Requirements packages
-# !pip install hume opencv-python matplotlib pillow
 import os
 import json
-from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+from PIL import Image, ImageDraw, ImageFont
 from hume import HumeBatchClient
 from hume.models.config import FaceConfig
 
@@ -54,44 +50,43 @@ class FaceEmotionAnalyzer:
                 print(f"Error opening image {image_path}: {e}")
                 continue
 
-            fig, ax = plt.subplots(1)
-            ax.imshow(image)
+            draw = ImageDraw.Draw(image)
+            font_size = 75
+            font = ImageFont.truetype("arial.ttf", font_size)
 
             face_count = 1
             for face in prediction['results']['predictions'][0]['models']['face']['grouped_predictions']:
                 for face_data in face['predictions']:
                     box = face_data['box']
-                    rect = patches.Rectangle(
-                        (box['x'], box['y']), box['w'], box['h'], linewidth=2, edgecolor='r', facecolor='none')
-                    ax.add_patch(rect)
+                    draw.rectangle(
+                        [(box['x'], box['y']), (box['x'] + box['w'], box['y'] + box['h'])],
+                        outline="red", width=20
+                    )
 
                     emotion, score = self.get_highest_emotion(
                         face_data['emotions'])
-                    plt.text(box['x'], box['y'], f'{face_count}: {emotion} ({score: .2f})',
-                             color="yellow", fontsize=8, weight='bold')
+                    draw.text((box['x'], box['y']), f'{face_count}: {emotion} ({score:.2f})',
+                              fill="yellow", font=font)
 
                     face_count += 1
 
-            plt.axis('off')
-            plt.title(f"Image {idx}")
+            image.save(f'image_with_emotion_{idx}.png', quality=95)
 
             for face_idx, face in enumerate(prediction['results']['predictions'][0]['models']['face']['grouped_predictions'], start=1):
                 for face_data_idx, face_data in enumerate(face['predictions'], start=1):
                     emotion, score = self.get_highest_emotion(
                         face_data['emotions'])
                     print(
-                        f'Person {face_idx}.{face_data_idx}: {emotion}({score: .2f})')
+                        f'Person {face_idx}.{face_data_idx}: {emotion}({score:.2f})')
 
-            plt.savefig(f'image_with_emotion_{idx}.png',
-                        bbox_inches='tight', pad_inches=0)
-            plt.close()
+            print(f"Image with emotions saved as image_with_emotion_{idx}.png")
 
 
 if __name__ == "__main__":
     api_key = "YOUR_API_KEY"
     filepaths = [
-        "C:\Study\CPP\HK9\AngleH\Heneiken\Beer\\0.jpg",
-        "C:\Study\CPP\HK9\AngleH\Heneiken\Beer\\1.jpg",
+        "D:\GIT\HUME\\BZ1A0408.jpg",
+        "D:\GIT\HUME\\BZ1A0464.jpg",
     ]
 
     analyzer = FaceEmotionAnalyzer(api_key, filepaths)
